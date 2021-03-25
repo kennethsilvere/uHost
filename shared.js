4b5c53bb3968ab0d08157bc5cd57cdcc4d468f0c
Kenneth Silvere <kennethsilvere93@gmail.com>
2021-03-25 13:18:36 +0530

    Use css animations & transitions, also convert code to scss and use sass to compile it to css

--- shared.js
+++ shared.js
@@ -1,43 +1,37 @@
-var backdrop = document.querySelector(".backdrop");
-var selectPlanButtons = document.querySelectorAll(".plan button");
-var hamburgerButton = document.querySelector(".toggle-button");
-var mobileNav = document.querySelector(".mobile-nav");
-var modal = document.querySelector(".modal");
+var backdrop = document.querySelector(".backdrop")
+var selectPlanButtons = document.querySelectorAll(".plan button")
+var hamburgerButton = document.querySelector(".toggle-button")
+var mobileNav = document.querySelector(".mobile-nav")
+var modal = document.querySelector(".modal")
 if (modal) {
-  var modalNoButton = modal.querySelector(".modal__action--negative");
+  var modalNoButton = modal.querySelector(".modal__action--negative")
 }
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", () => {
-    // backdrop.style.display = "block";
-    // modal.style.display = "block";
-    backdrop.classList.add("open");
-    modal.classList.add("open");
-  });
+    modal.classList.add("open")
+    backdrop.style.display = "block"
+    setTimeout(() => backdrop.classList.add("open"), 200)
+  })
 }
 if (modalNoButton) {
   modalNoButton.addEventListener("click", () => {
-    //   backdrop.style.display = "none";
-    //   modal.style.display = "none";
-    backdrop.classList.remove("open");
-    modal.classList.remove("open");
-  });
+    backdrop.classList.remove("open")
+    modal.classList.remove("open")
+    setTimeout(() => (backdrop.style.display = "none"), 200)
+  })
 }
 
 backdrop.addEventListener("click", () => {
-  //   backdrop.style.display = "none";
-  //   modal.style.display = "none";
-  //   mobileNav.style.display = "none";
-  backdrop.classList.remove("open");
-  mobileNav.classList.remove("open");
+  backdrop.classList.remove("open")
+  mobileNav.classList.remove("open")
+  setTimeout(() => (backdrop.style.display = "none"), 200)
   if (modal) {
-    modal.classList.remove("open");
+    modal.classList.remove("open")
   }
-});
+})
 
 hamburgerButton.addEventListener("click", () => {
-  //   backdrop.style.display = "block";
-  //   mobileNav.style.display = "block";
-  backdrop.classList.add("open");
-  mobileNav.classList.add("open");
-});
+  backdrop.classList.add("open")
+  mobileNav.classList.add("open")
+})
